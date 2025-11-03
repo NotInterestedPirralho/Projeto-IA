15c1f903a9c8754082d96981f144d19f8c89bba7
Filipe323 <a27927@alunos.ipca.pt>
2025-11-03 17:45:49 +0000

    Death Countdown

--- Assets/Scripts/Core/PlayerSetup.cs
+++ Assets/Scripts/Core/PlayerSetup.cs
@@ -18,10 +18,14 @@ public class PlayerSetup : MonoBehaviour
     {
         movement.enabled = true;
         camara.SetActive(true);
-        // inicia o zoom quando a câmara for ativada
-        var zoom = camara.GetComponent<CameraIntroZoomFOV>();
-        if (zoom != null)
-            zoom.StartZoom();
+        // inicia o zoom quando a câmara for    ativada
+        // ativa a câmara e inicia o zoom dinâmico (se existir)
+        camara.SetActive(true);
+
+        var zoomDynamic = camara.GetComponent<CameraDynamicZoom>();
+        if (zoomDynamic != null)
+            zoomDynamic.enabled = true;
+
 
         // Enable combat system for the local player only
         if (combat != null)
