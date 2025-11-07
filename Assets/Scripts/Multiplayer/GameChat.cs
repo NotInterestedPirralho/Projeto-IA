57fdae61ff1986143944f08b4cce5c36a5df1e46
NotInterestedPirralho <a27920@alunos.ipca.pt>
2025-11-07 15:24:06 +0000

    chat

--- Assets/Scripts/Multiplayer/GameChat.cs
+++ Assets/Scripts/Multiplayer/GameChat.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -17,33 +18,44 @@ public class GameChat : MonoBehaviour
     {
 
 
-        if (Input.GetKeyDown(KeyCode.Y))
+        if (Input.GetKeyDown(KeyCode.T) && !isInputFieldToggled)
         {
-            if (isInputFieldToggled)
-            {
-                isInputFieldToggled = false;
+            isInputFieldToggled = true;
+            inputField.Select();
+            inputField.ActivateInputField();
 
-                UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+            Debug.Log("Toggled on");
+            
+        }
 
-                Debug.Log("Toggled off");
-            }
-            else
-            {
-                isInputFieldToggled = true;
-                inputField.Select();
-                inputField.ActivateInputField();
+        if (Input.GetKeyDown(KeyCode.Escape) && isInputFieldToggled)
+        {
+            isInputFieldToggled = false;
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
 
-                Debug.Log("Toggled on");
-            }
+            Debug.Log("Toggled off");
         }
 
         //Sending a message
-        /*if ((Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) && isInputFieldToggled && !InputField.text.IsNullOrEmpty())
+        if ((Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) && isInputFieldToggled && !inputField.text.IsNullOrEmpty())
         {
             //sending a message
 
-            Debug.Log("Message sent");
+            string messageToSend = $"Player {PhotonNetwork.LocalPlayer.NickName}: {inputField.text}";
 
-        }*/
+            GetComponent<PhotonView>().RPC("SendChatMessage", RpcTarget.AllBuffered, messageToSend);
+
+            inputField.text = "";
+            isInputFieldToggled = false;
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+
+            Debug.Log("Message sent");
+        }
+    }
+
+    [PunRPC]
+    public void SendChatMessage(string _message)
+    {
+        chatText.text = chatText + "\n" + _message;
     }
 }
